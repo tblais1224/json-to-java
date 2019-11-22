3d2063fddd31329cca28e668e7131d5f716e8618
tblais1224 <thomasblais.student@careerdevs.com>
2019-11-22 16:56:40 -0500

    trying to get the compiler version working

---
+++ src/main/java/jsonToJava.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class jsonToJava {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = null;
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/business", "root", "root" );
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("select * from customerinfo where PurchasedDate=CURDATE() and Location " +
+                "='Asia';");
+        rs.next();
+        while (rs.next()) {
+            CustomerDetails customer =  new CustomerDetails();
+            customer.setCourseName(rs.getString(1));
+            customer.setPurchaseDate(rs.getString(2));
+            customer.setAmount(rs.getInt(3));
+            customer.setLocation(rs.getString(4));
+
+            System.out.println(customer.getCourseName());
+            System.out.println(customer.getPurchaseDate());
+            System.out.println(customer.getAmount());
+            System.out.println(customer.getLocation());
+        }
+        connection.close();
+    }
+}
---
+++ target/classes/CustomerDetails.java
@@ -0,0 +1,39 @@
+
+public class CustomerDetails{
+    private String courseName;
+    private String purchaseDate;
+    private int amount;
+    private String location;
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
